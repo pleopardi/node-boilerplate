fbe136878822581464c1f6ef5c2c38c858b56e0e
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-20 18:52:07 +0200

    chore(.eslintrc):
    migrate to ES6 module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
-module.exports = {
-    "env": {
-        "es6": true,
-        "jest/globals": true,
-        "node": true
+export default {
+    env: {
+      es6: true,
+      jest: true,
+      node: true
     },
-    "extends": ["eslint:recommended", "plugin:jest/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: ["eslint:recommended", "plugin:jest/recommended"],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly"
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 2019,
+      sourceType: "module"
     },
-    "plugins": ["jest", "prettier"],
-    "rules": {
-        "prettier/prettier": "error"
+    plugins: ["jest", "prettier"],
+    rules: {
+      "prettier/prettier": "error"
     }
-}; 
+  };
+  
