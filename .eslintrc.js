d24c93c454046f4b8d6a9f6cffbbab56c803ad63
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-20 21:37:49 +0200

    chore(.eslintrc):
    revert to CommonJS module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
-export default {
-    env: {
-      es6: true,
-      jest: true,
-      node: true
-    },
-    extends: ["eslint:recommended", "plugin:jest/recommended"],
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-      ecmaVersion: 2019,
-      sourceType: "module"
-    },
-    plugins: ["jest", "prettier"],
-    rules: {
-      "prettier/prettier": "error"
-    }
-  };
-  
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:jest/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module"
+  },
+  plugins: ["jest", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
